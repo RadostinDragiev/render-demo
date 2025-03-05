a15edebf99870f02336a2603f0e4da97efd3072c
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2025-03-05 15:56:03 +0200

    Env variable test

--- src/main/java/com/renderdemo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/renderdemo/service/impl/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package com.renderdemo.service.impl;
 import com.renderdemo.service.DemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -12,9 +13,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class DemoServiceImpl implements DemoService {
 
+    @Value("render.demo")
+    private String demo;
+
     @Override
     public Map<String, String> getMap() {
         log.warn("GetMap method called");
+        log.warn("This is my env variable test -> " + demo);
         return Map.of("Gosh", "Sofia",
                 "Pesho", "Troyan",
                 "Ivan", "Varna");
